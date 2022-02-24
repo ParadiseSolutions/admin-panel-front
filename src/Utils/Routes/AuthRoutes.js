ef7382f8632bd3efb6afbaacbfc5787a1539e17b
carlosjs00 <carlos@jstourandtravel.com>
2022-02-24 16:50:44 -0600

    reset pass page

--- src/Utils/Routes/AuthRoutes.js
+++ src/Utils/Routes/AuthRoutes.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import Login from '../../Pages/Auth/Login'
 import ForgotPassword from '../../Pages/Auth/ForgotPassword'
-
+import ResetPassword from '../../Pages/Auth/ResetPassword'
 const AuthRoutes = () => {
     return (
         <div>
             <Switch>
                 <Route exact path='/login' component={Login} /> 
                 <Route exact path='/login/forgot-password' component={ForgotPassword} /> 
+                <Route exact path='/login/reset-password' component={ResetPassword} /> 
                 <Redirect to='/login' />
             </Switch>
         </div>
