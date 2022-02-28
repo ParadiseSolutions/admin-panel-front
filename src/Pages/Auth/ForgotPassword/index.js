460c4da78d4fa49b9f243ccc4443873287a8e6c7
carlosjs00 <carlos@jstourandtravel.com>
2022-02-28 08:30:04 -0600

    validation form

--- src/Pages/Auth/ForgotPassword/index.js
+++ src/Pages/Auth/ForgotPassword/index.js
@@ -3,29 +3,65 @@ import LoginImage from "../../../Components/Assets/images/login-girl.png";
 import Alpaca from "../../../Components/Assets/images/alpaca.png";
 import ParadiseLogo from "../../../Components/Assets/images/paradise-logo.png";
 import SweetAlert from "react-bootstrap-sweetalert";
-import { Formik } from "formik";
 import { ForgotPasswordAction } from "../../../Utils/Redux/Actions/ForgotPassword";
+import { Label, Input, FormFeedback, Form } from "reactstrap";
+// Formik validation
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 const ForgotPassword = () => {
   const [custom_div1, setcustom_div1] = useState(false);
-  const [custom_div2, setcustom_div2] = useState(false);
-  const onSubmit = (values) => {
-    console.log(values);
+  // Form validation
+  const [error, setError] = useState(false);
+  const validationType = useFormik({
+    // enableReinitialize : use this flag when initial values needs to be changed
+    enableReinitialize: true,
 
-    let data = {
-      email: values.email,
-    };
-    ForgotPasswordAction(data)
-      .then((resp) => {
-        if (resp.data.status === 200) {
-          setcustom_div1(true);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        setcustom_div2(true)
-      });
-  };
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email("Must be a valid Email")
+        .max(255)
+        .required("Email is required"),
+    }),
+    onSubmit: (values) => {
+      console.log("values", values);
+      let data = {
+        email: values.email,
+      };
+      ForgotPasswordAction(data)
+        .then((resp) => {
+          if (resp.data.status === 200) {
+            setcustom_div1(true);
+            setError(false);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(true);
+        });
+    },
+  });
+
+  // const onSubmit = (values) => {
+  //   console.log(values);
+
+  //   let data = {
+  //     email: values.email,
+  //   };
+  //   ForgotPasswordAction(data)
+  //     .then((resp) => {
+  //       if (resp.data.status === 200) {
+  //         setcustom_div1(true);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setcustom_div2(true)
+  //     });
+  // };
   return (
     <section className="vh-100">
       <div className="container-fluid">
@@ -69,145 +105,123 @@ const ForgotPassword = () => {
             <div className="row justify-content-center">
               <div className="col-7 mt-3">
                 <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                  <Formik
-                    initialValues={{
-                      email: "",
+                  <Form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      validationType.handleSubmit();
+                      return false;
                     }}
-                    enableReinitialize
-                    onSubmit={(values, { setSubmitting }) => {
-                      onSubmit(values);
-                      setSubmitting(false);
-                    }}
+                    className="custom-validation"
                   >
-                    {({
-                      values,
-                      errors,
-                      handleChange,
-                      handleSubmit,
-                      handleBlur,
-                      isSubmitting,
-                      setFieldValue,
-                    }) => {
-                      return (
-                        <form onSubmit={handleSubmit} className="col-12">
-                          <div className="row justify-content-center mb-4">
-                            <div className="col-5">
-                              <p
-                                style={{
-                                  color: "#74788D",
-                                  fontSize: "18px",
-                                  marginLeft: "8px",
-                                }}
-                              >
-                                Reset Password
-                              </p>
-                            </div>
-                            <div className="col-12">
-                              <div
-                                className="form-control"
-                                style={{
-                                  backgroundColor: "#C7E3EB",
-                                  border: "none",
-                                }}
-                              >
-                                <p
-                                  style={{
-                                    padding: "5px",
-                                    marginBottom: 0,
-                                    marginLeft: "1rem",
-                                    color: "#2986A4",
-                                  }}
-                                >
-                                  Enter your Email and instructions will be sent
-                                  to you!
-                                </p>
-                              </div>
-                            </div>
-                          </div>
+                    <div className="row justify-content-center mb-4">
+                      <div className="col-5">
+                        <p
+                          style={{
+                            color: "#74788D",
+                            fontSize: "18px",
+                            marginLeft: "8px",
+                          }}
+                        >
+                          Reset Password
+                        </p>
+                      </div>
 
-                          <div className="form-outline mb-4">
-                            <label
-                              className="form-label"
-                              htmlFor="form2Example28"
-                            >
-                              Email
-                            </label>
-                            <input
-                              type="email"
-                              name="email"
-                              value={values.email}
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              id="form2Example28"
-                              className="form-control"
-                            />
-                          </div>
+                      <div className="col-12">
+                        <div
+                          className="form-control"
+                          style={{
+                            backgroundColor: "#C7E3EB",
+                            border: "none",
+                          }}
+                        >
+                          <p
+                            style={{
+                              padding: "5px",
+                              marginBottom: 0,
+                              marginLeft: "1rem",
+                              color: "#2986A4",
+                            }}
+                          >
+                            Enter your Email and instructions will be sent to
+                            you!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
 
-                          <div className="mt-3 col-12">
-                            <button
-                              className="btn btn-info btn-lg btn-block"
-                              style={{
-                                width: "100%",
-                                backgroundColor: "#3DC7F4",
-                                padding: "5px",
-                                border: "none",
-                              }}
-                              type="submit"
-                            >
-                              Reset
-                            </button>
-                          </div>
+                    <div className="form-outline mb-4">
+                      <Label className="form-label">Email</Label>
+                      <Input
+                        name="email"
+                        placeholder="Enter Valid Email"
+                        type="email"
+                        onChange={validationType.handleChange}
+                        onBlur={validationType.handleBlur}
+                        value={validationType.values.email || ""}
+                        invalid={
+                          validationType.touched.email &&
+                          validationType.errors.email
+                            ? true
+                            : false
+                        }
+                      />
+                      {validationType.touched.email &&
+                      validationType.errors.email ? (
+                        <FormFeedback type="invalid">
+                          {validationType.errors.email}
+                        </FormFeedback>
+                      ) : null}
+                    </div>
 
-                          <div className="mt-5 row justify-content-center">
-                            <div
-                              className="col-5"
-                              style={{ marginLeft: "1rem" }}
-                            >
-                              <p style={{ fontSize: "14.4px" }}>
-                                Remember It ?{" "}
-                                <a
-                                  href="/login"
-                                  style={{ fontSize: "14.4px" }}
-                                  className="link-info mx-1"
-                                >
-                                  Sign in
-                                </a>
-                              </p>
-                            </div>
-                          </div>
-                          <div className="mt-5 row justify-content-center">
-                            <div
-                              className="col-5"
-                              style={{ marginLeft: "1rem" }}
-                            >
-                              <p style={{ fontSize: "14.4px" }}>
-                                Remember It ?{" "}
-                                <a
-                                  href="/reset-password"
-                                  style={{ fontSize: "14.4px" }}
-                                  className="link-info mx-1"
-                                >
-                                  reset
-                                </a>
-                              </p>
-                            </div>
-                          </div>
-                          <div className=" row justify-content-center">
-                            <div
-                              className="col-4 mt-2"
-                              style={{ marginLeft: "1rem" }}
-                            >
-                              <img
-                                src={ParadiseLogo}
-                                alt="logo"
-                                style={{ width: "100px" }}
-                              />
-                            </div>
-                          </div>
-                        </form>
-                      );
-                    }}
-                  </Formik>
+                    <div className="mt-3 col-12">
+                      <button
+                        className="btn btn-info btn-lg btn-block"
+                        style={{
+                          width: "100%",
+                          backgroundColor: "#3DC7F4",
+                          padding: "5px",
+                          border: "none",
+                        }}
+                        type="submit"
+                      >
+                        Reset
+                      </button>
+                    </div>
+                    {error ? (
+                      <div className="mt-2">
+                        <p style={{ color: "red", fontSize: "14.4px" }}>
+                          *Email address not registered.
+                        </p>
+                      </div>
+                    ) : null}
+                    <div className="mt-5 row justify-content-center">
+                      <div className="col-5" style={{ marginLeft: "1rem" }}>
+                        <p style={{ fontSize: "14.4px" }}>
+                          Remember It ?{" "}
+                          <a
+                            href="/login"
+                            style={{ fontSize: "14.4px" }}
+                            className="link-info mx-1"
+                          >
+                            Sign in
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                   
+                    <div className=" row justify-content-center">
+                      <div
+                        className="col-4 mt-2"
+                        style={{ marginLeft: "1rem" }}
+                      >
+                        <img
+                          src={ParadiseLogo}
+                          alt="logo"
+                          style={{ width: "100px" }}
+                        />
+                      </div>
+                    </div>
+                  </Form>
                 </div>
               </div>
             </div>
@@ -219,8 +233,6 @@ const ForgotPassword = () => {
                 timeout={2000}
                 style={{
                   position: "absolute",
-                  top: "0px",
-                  right: "0px",
                 }}
                 showCloseButton={false}
                 showConfirm={false}
@@ -230,23 +242,7 @@ const ForgotPassword = () => {
                 }}
               ></SweetAlert>
             ) : null}
-            {custom_div2 ? (
-              <SweetAlert
-                title="Email address not registered."
-                timeout={2000}
-                style={{
-                  position: "absolute",
-                  top: "0px",
-                  right: "0px",
-                }}
-                showCloseButton={false}
-                showConfirm={false}
-                error
-                onConfirm={() => {
-                  setcustom_div2(false);
-                }}
-              ></SweetAlert>
-            ) : null}
+
             <img
               src={LoginImage}
               alt="LoginImage"
