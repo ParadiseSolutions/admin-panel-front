f7dd2e1f9a232099fd46ab4423291c12560cecae
carlosjs00 <carlos@jstourandtravel.com>
2022-03-28 09:02:32 -0600

    url changes

--- src/Utils/API/index.js
+++ src/Utils/API/index.js
@@ -8,7 +8,7 @@ export const getStorageSync = (key) => {
 
 const tokenData = JSON.parse(getStorageSync("token"));
 const bearerToken = tokenData ? `Bearer ${tokenData.token}` : '';
-export const API_URL = `https://api.paradisesolutions.com/api`;
+export const API_URL = `https://apitest.paradisesolutions.com/api`;
 
 export const options = {
   "Content-Type": "application/json",
