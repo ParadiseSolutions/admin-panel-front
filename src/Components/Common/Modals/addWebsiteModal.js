c9178ecc8d038602d893f0aef0e0e8a906bf9799
williamm-js <william@jstourandtravel.com>
2022-03-29 10:48:36 -0600

    completed add new website as a popup

--- src/Components/Common/Modals/addWebsiteModal.js
+++ src/Components/Common/Modals/addWebsiteModal.js
@@ -114,7 +114,8 @@ const AddWebsiteModal = ({ addModal, setAddModal, onClickAddNewWebsite }) => {
         >
           <Row>
             <Col className="col-12 mx-5">
-              <Col lg={5}>
+              <Row>
+              <Col lg={3}>
                 <div className="form-outline mb-4">
                   <Label className="form-label">Website Name</Label>
                   <Input
@@ -128,33 +129,167 @@ const AddWebsiteModal = ({ addModal, setAddModal, onClickAddNewWebsite }) => {
                   />
                   {validationType.touched.company_name && validationType.errors.company_name ? <FormFeedback type="invalid">{validationType.errors.company_name}</FormFeedback> : null}
                 </div>
+               
               </Col>
-
-              <Col className="col-5 mx-4">
+              
+              <Col lg={2}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">2-Digit Code</Label>
+                  <Input
+                    name="code"
+                    placeholder=""
+                    type="text"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.code || ""}
+                    invalid={validationType.touched.code && validationType.errors.code ? true : false}
+                  />
+                  {validationType.touched.code && validationType.errors.code ? <FormFeedback type="invalid">{validationType.errors.code}</FormFeedback> : null}
+                </div>
+               
+              </Col>
+              <Col lg={5}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">Root Folder</Label>
+                  <Input
+                    name="root_ccount"
+                    placeholder=""
+                    type="text"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.root_ccount || ""}
+                    invalid={validationType.touched.root_ccount && validationType.errors.root_ccount ? true : false}
+                  />
+                  {validationType.touched.root_ccount && validationType.errors.root_ccount ? <FormFeedback type="invalid">{validationType.errors.root_ccount}</FormFeedback> : null}
+                </div>
+              </Col>
+              </Row>
+              <Row>
+              <Col lg={5}>
                 <div className="form-outline mb-4">
                   <Label className="form-label">Website URL</Label>
                   <Input
                     name="url"
                     placeholder=""
-                        type="text"
-                        onChange={validationType.handleChange}
-                        onBlur={validationType.handleBlur}
-                        value={validationType.values.url || ""}
-                        invalid={
-                          validationType.touched.url &&
-                          validationType.errors.url
-                            ? true
-                            : false
-                        }
-                      />
-                      {validationType.touched.url &&
-                      validationType.errors.url ? (
-                        <FormFeedback type="invalid">
-                          {validationType.errors.url}
-                        </FormFeedback>
-                      ) : null}
+                    type="text"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.url || ""}
+                    invalid={validationType.touched.url && validationType.errors.url ? true : false}
+                  />
+                  {validationType.touched.url && validationType.errors.url ? <FormFeedback type="invalid">{validationType.errors.url}</FormFeedback> : null}
                 </div>
               </Col>
+              <Col lg={5}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">User Folder</Label>
+                  <Input
+                    name="user_folder"
+                    placeholder=""
+                    type="text"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.user_folder || ""}
+                    invalid={validationType.touched.user_folder && validationType.errors.user_folder ? true : false}
+                  />
+                  {validationType.touched.user_folder && validationType.errors.user_folder ? <FormFeedback type="invalid">{validationType.errors.user_folder}</FormFeedback> : null}
+                </div>
+              </Col>
+              </Row>
+              <Row>
+              <Col lg={5}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">Website Domain</Label>
+                  <Input
+                    name="domain"
+                    placeholder=""
+                    type="text"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.domain || ""}
+                    invalid={validationType.touched.domain && validationType.errors.domain ? true : false}
+                  />
+                  {validationType.touched.domain && validationType.errors.domain ? <FormFeedback type="invalid">{validationType.errors.domain}</FormFeedback> : null}
+                </div>
+              </Col>
+
+              <Col lg={2}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">Accent Color</Label>
+                  <Input
+                    name="accent_color"
+                    placeholder=""
+                    type="color"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.accent_color || ""}
+                    invalid={validationType.touched.accent_color && validationType.errors.accent_color ? true : false}
+                  />
+                  {validationType.touched.accent_color && validationType.errors.accent_color ? <FormFeedback type="invalid">{validationType.errors.accent_color}</FormFeedback> : null}
+                </div>
+              </Col>
+
+              <Col lg={2}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">Primary Color</Label>
+                  <Input
+                    name="primary_color"
+                    placeholder=""
+                    type="color"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.primary_color || ""}
+                    invalid={validationType.touched.primary_color && validationType.errors.primary_color ? true : false}
+                  />
+                  {validationType.touched.primary_color && validationType.errors.primary_color ? <FormFeedback type="invalid">{validationType.errors.primary_color}</FormFeedback> : null}
+                </div>
+              </Col>
+             
+              </Row>
+              <Row>
+              <Col lg={5}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">cPanel Account</Label>
+                  <Input
+                    name="cpanel_account"
+                    placeholder=""
+                    type="text"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.cpanel_account || ""}
+                    invalid={validationType.touched.cpanel_account && validationType.errors.cpanel_account ? true : false}
+                  />
+                  {validationType.touched.cpanel_account && validationType.errors.cpanel_account ? <FormFeedback type="invalid">{validationType.errors.cpanel_account}</FormFeedback> : null}
+                </div>
+              </Col>
+              <Col lg={2}>
+                <div className="form-outline mb-4">
+                  <Label className="form-label">Secondary Color</Label>
+                  <Input
+                    name="secondary_color"
+                    placeholder=""
+                    type="color"
+                    onChange={validationType.handleChange}
+                    onBlur={validationType.handleBlur}
+                    value={validationType.values.secondary_color || ""}
+                    invalid={validationType.touched.secondary_color && validationType.errors.secondary_color ? true : false}
+                  />
+                  {validationType.touched.secondary_color && validationType.errors.secondary_color ? <FormFeedback type="invalid">{validationType.errors.secondary_color}</FormFeedback> : null}
+                </div>
+              </Col>
+              </Row>
+              <Row>
+                <Col className="col-10 mx-4 mt-2 d-flex justify-content-end">
+                  <Button
+                    type="submit"
+                    style={{ backgroundColor: "#F6851F", border: "none" }}
+                    className="waves-effect waves-light mb-3 btn btn-success"
+                  >
+                    <i className="mdi mdi-plus me-1" />
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Form>
