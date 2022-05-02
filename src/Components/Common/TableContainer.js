ecb9ea29d532887c71891d2f8d93bb0273bb1f14
williamm-js <william@jstourandtravel.com>
2022-05-02 11:20:16 -0600

    completed locations module

--- src/Components/Common/TableContainer.js
+++ src/Components/Common/TableContainer.js
@@ -16,6 +16,7 @@ const TableContainer = ({
   usersTable,
   cartsTable, 
   categoriesTable,
+  locationsTable,
   paymentsTable,
   handleUserClicks, 
   onClickAddNew, 
@@ -23,6 +24,7 @@ const TableContainer = ({
   onClickAddNewWebsite,
   onClickAddNewCart,
   onClickAddTourType,
+  onClickAddLocation,
   onClickAddNewPayment
  }) => {
   const {
@@ -101,6 +103,20 @@ const TableContainer = ({
 						</div>
 					</Col>
 				)}
+
+{locationsTable && (
+					<Col sm="8">
+						<div className="text-sm-end">
+							
+								<Button type="button" style={{ backgroundColor: "#F6851F", border: "none" }} className="waves-effect waves-light mb-3 btn btn-success" onClick={() => onClickAddLocation()}>
+									<i className="mdi mdi-plus me-1" />
+									Add New Location
+								</Button>
+							
+						</div>
+					</Col>
+				)}
+
 				{websitesTable && (
 					<Col sm="8">
 						<div className="text-sm-end">
@@ -260,6 +276,47 @@ const TableContainer = ({
 			)}
 
 {categoriesTable && (
+				<div className="table-responsive">
+					<Table bordered hover {...getTableProps()} className="react_table">
+						<thead className="table-nowrap">
+							{headerGroups.map((headerGroup) => (
+								<tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
+									{headerGroup.headers.map((column) => (
+										<th key={column.id}>
+											<div {...column.getSortByToggleProps()}>
+												{column.render("Header")}
+												{/* {generateSortingIndicator(column)} */}
+											</div>
+											{/* <Filter column={column} /> */}
+										</th>
+									))}
+								</tr>
+							))}
+						</thead>
+
+						<tbody {...getTableBodyProps()}>
+							{page.map((row) => {
+								prepareRow(row);
+								return (
+									<Fragment key={row.getRowProps().key}>
+										<tr>
+											{row.cells.map((cell) => {
+												return (
+													<td key={cell.id} {...cell.getCellProps()}>
+														{cell.render("Cell")}
+													</td>
+												);
+											})}
+										</tr>
+									</Fragment>
+								);
+							})}
+						</tbody>
+					</Table>
+				</div>
+			)}
+
+{locationsTable && (
 				<div className="table-responsive">
 					<Table bordered hover {...getTableProps()} className="react_table">
 						<thead className="table-nowrap">
---
+++ src/Pages/Locations/index.js
@@ -0,0 +1,169 @@
+import { useEffect, useMemo, useState } from "react";
+import { Container } from "reactstrap";
+import { locationsData } from "../../Utils/Redux/Actions/LocationsActions";
+import { useSelector, useDispatch } from "react-redux";
+import { Row, Col, Card, CardBody, UncontrolledTooltip } from "reactstrap";
+import { Link } from "react-router-dom";
+import TableContainer from "../../Components/Common/TableContainer";
+import { Name, Active } from "./LocationsCols";
+import Swal from "sweetalert2";
+import { locationDelete } from "../../Utils/API/Locations";
+import AddLocationModal from "../../Components/Common/Modals/LocationsModals/addLocationModal";
+import UpdateLocationModal from "../../Components/Common/Modals/LocationsModals/updateLocationModal";
+
+const Locations = () => {
+
+	const [locationsId, setLocationsId] = useState(false);
+	const dispatch = useDispatch();
+
+	//locations request
+
+	useEffect(() => {
+		const locationsRequest = () => dispatch(locationsData());
+		locationsRequest();
+	}, [dispatch]);
+
+	//saving locations data into variable
+	
+	const data = useSelector((state) => state.locations.locations.data);
+	console.log("eeeeeeew", data)
+
+		//delete Location
+		const locationsRequest = () => dispatch(locationsData());
+		const onDelete = (locationData) => {
+			Swal.fire({
+				title: "Delete Location?",
+				icon: "question",
+				text: `Do you want delete ${locationData.name}`,
+				showCancelButton: true,
+				confirmButtonText: "Yes",
+				confirmButtonColor: "#F38430",
+				cancelButtonText: "Cancel",
+			}).then((resp) => {
+				if (resp.isConfirmed) {
+					locationDelete(locationData.id)
+						.then((resp) => {
+							// const locationsRequest = () => dispatch(locationsData());
+							locationsRequest();
+							Swal.fire("Deleted!", "Location has been deleted.", "success");
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				}
+			});
+		};
+
+// Locations columns
+
+const columns = useMemo(() => [
+	{
+		Header: "Name",
+		accessor: "name",
+		disableFilters: true,
+		filterable: false,
+		Cell: (cellProps) => {
+			return <Name {...cellProps} />;
+		},
+	},
+	{
+		Header: "Active",
+		accessor: "active",
+		disableFilters: true,
+		filterable: false,
+		Cell: (cellProps) => {
+			return <Active {...cellProps} />;
+		},
+	},
+	{
+		Header: "Action",
+		accessor: "action",
+		disableFilters: true,
+		Cell: (cellProps) => {
+		  //const locationData = cellProps.row.original;
+		  return (
+			<div className="d-flex gap-3">
+			  
+			  <div
+			   
+				className="text-success"
+				onClick={() => {
+				  setEditModal(true)
+				  const locationData = cellProps.row.original;
+				  setLocationsId(locationData.id);
+				}}
+			  >
+				<i className="mdi mdi-pencil font-size-18" id="edittooltip" />
+				<UncontrolledTooltip placement="top" target="edittooltip">
+				  Edit
+				</UncontrolledTooltip>
+			  </div>
+			  <Link
+				to="#"
+				className="text-danger"
+				onClick={() => {
+				  const locationData = cellProps.row.original;
+				  // setconfirm_alert(true);
+				  onDelete(locationData);
+				}}
+			  >
+				<i className="mdi mdi-delete font-size-18" id="deletetooltip" />
+				<UncontrolledTooltip placement="top" target="deletetooltip">
+				  Delete
+				</UncontrolledTooltip>
+			  </Link>
+			</div>
+		  );
+		},
+	  }
+]);
+
+//modal new
+const [ editModal, setEditModal] = useState(false)
+const [addModal, setAddModal] = useState(false);
+const onClickAddLocation = () => {
+	setAddModal(!addModal);
+};
+
+const onClickEditLocation = () => {
+	setEditModal(!editModal);
+};
+	return (
+		<>
+			<div className="page-content">
+				<Container fluid>
+				<div className=" mx-5">
+						<h1 className="display-5 fw-bold cursor-pointer" style={{ color: "#3DC7F4" }}>
+							LOCATIONS
+						</h1>
+					</div>
+
+					<Row>
+						<Col xs="12">
+							<Card>
+								<CardBody>
+									{data ? (
+										<TableContainer
+											columns={columns}
+											data={data}
+											isGlobalFilter={true}
+											locationsTable={true}
+											isAddOrder={true}
+											onClickAddLocation={onClickAddLocation}
+											onClickEditLocation={onClickEditLocation}
+											// // handleOrderClicks={handleOrderClicks}
+										/>
+									) : null}
+								</CardBody>
+							</Card>
+						</Col>
+					</Row>
+					<AddLocationModal addModal={addModal} setAddModal={setAddModal} onClickAddLocation={onClickAddLocation} />
+					<UpdateLocationModal locationId = {locationsId} editModal={editModal} setEditModal={setEditModal} onClickEditLocation={onClickEditLocation} />
+				</Container>
+			</div>
+		</>
+	);
+};
+
+export default Locations;
