5bb0fe248cce8fb3f51d1d989905813d2f1d8106
cristyjs <133432299+robertoparadise@users.noreply.github.com>
2023-05-12 14:43:14 -0600

    Test

--- src/Pages/Departments/index.js
+++ src/Pages/Departments/index.js
@@ -144,7 +144,7 @@ const Departments = () => {
       <div className="page-content">
         <Container fluid>
           <div className=" mx-5">
-            <h1 className="display-5 fw-bold cursor-pointer" style={{ color: "#3DC7F4" }}>
+            <h1 className="display-5 fw-bold cursor-pointer sheep" style={{ color: "#3DC7F4" }}>
               DEPARTMENTS
             </h1>
           </div>
