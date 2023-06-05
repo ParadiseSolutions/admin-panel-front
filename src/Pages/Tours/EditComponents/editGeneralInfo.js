db97b7bb6aa80e138cadbadcfc8f9a5054f7d9e0
cristyjs <133432299+robertoparadise@users.noreply.github.com>
2023-06-05 16:56:59 -0600

    Started Modals Modifications
    Some adjustments to the tours section and the modifications to the modals has been started. Only the addNewProduct.js is finished. The rest of the modals are in progress.

--- src/Pages/Tours/EditComponents/editGeneralInfo.js
+++ src/Pages/Tours/EditComponents/editGeneralInfo.js
@@ -163,7 +163,7 @@ const EditGeneralInformation = ({ tourData, toggle }) => {
             <Row className="mb-3">
               <Col className="col-12">
                 <div className="p-3" style={{ backgroundColor: "#E9F4FF" }}>
-                  <p className="fs-4 fw-bold text-uppercase text-dark mb-0">
+                  <p className="fs-5 fw-bold text-uppercase text-dark mb-0">
                     TOUR SETTINGS
                   </p>
                 </div>              
@@ -305,7 +305,7 @@ const EditGeneralInformation = ({ tourData, toggle }) => {
             <Col className="col-12">
             <div className="p-3" style={{ backgroundColor: "#FFEFDE" }}>
                 <p
-                className="fs-4 fw-bold text-uppercase text-dark mb-0"
+                className="fs-5 fw-bold text-uppercase text-dark mb-0"
                 
               >
                 TOUR DETAILS
