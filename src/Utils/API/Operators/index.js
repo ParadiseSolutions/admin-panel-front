dc9af268f4636a5712a9cbd8f30a8ab3427d0e4b
carlosjs00 <98489442+carlosjs00@users.noreply.github.com>
2024-08-28 10:04:35 -0600

    new tables

--- src/Utils/API/Operators/index.js
+++ src/Utils/API/Operators/index.js
@@ -35,6 +35,18 @@ export const deleteOperatorAPI = (id) => {
     const url = `${API_URL}/vouchers/list/operators/${id}`;
     return axios.get(url, {headers:options})
   };
+  export const getMeetingLocationTable = (id) => {
+    const url = `${API_URL}/vouchers/meeting_locations/operators/${id}`;
+    return axios.get(url, {headers:options})
+  };
+  export const getBoatLocationTable = (id) => {
+    const url = `${API_URL}/vouchers/boat_locations/operators/${id}`;
+    return axios.get(url, {headers:options})
+  };
+  export const getRestrictionsTable = (id) => {
+    const url = `${API_URL}/vouchers/restrictions/operators/${id}`;
+    return axios.get(url, {headers:options})
+  };
   export const getExtraFee = () => {
     const url = `${API_URL}/vouchers/feeTypes`;
     return axios.get(url, {headers:options})
@@ -67,14 +79,68 @@ export const deleteOperatorAPI = (id) => {
       headers: options,
     });
   };
+  export const postLocationFee = (body) => {
+    const url = `${API_URL}/vouchers/addMeetingLocation`;
+    return axios.post (url, body, {
+      headers: options,
+    });
+  };
+  export const postLocationBoat = (body) => {
+    const url = `${API_URL}/vouchers/addBoatLocation`;
+    return axios.post (url, body, {
+      headers: options,
+    });
+  };
+  export const postRestriction = (body) => {
+    const url = `${API_URL}/vouchers/addRestriction`;
+    return axios.post (url, body, {
+      headers: options,
+    });
+  };
   export const putExtraFee = (id, body) => {
     const url = `${API_URL}/vouchers/updateExtraFee/${id}`;
     return axios.put (url, body, {
       headers: options,
     });
   };
+  export const putLocationFee = (id, body) => {
+    const url = `${API_URL}/vouchers/updateMeetingLocation/${id}`;
+    return axios.put (url, body, {
+      headers: options,
+    });
+  };
+  export const putBoatLocationFee = (id, body) => {
+    const url = `${API_URL}/vouchers/updateBoatLocation/${id}`;
+    return axios.put (url, body, {
+      headers: options,
+    });
+  };
+  export const putRestriction = (id, body) => {
+    const url = `${API_URL}/vouchers/updateRestriction/${id}`;
+    return axios.put (url, body, {
+      headers: options,
+    });
+  };
   export const deleteExtraFee = (id) => {
     const url = `${API_URL}/vouchers/deleteExtraFee/operators/${id}`;
+    return axios.delete (url, {
+      headers: options,
+    });
+  };
+  export const deleteMeetingLocation = (id) => {
+    const url = `${API_URL}/vouchers/deleteMeetingLocation/${id}`;
+    return axios.delete (url, {
+      headers: options,
+    });
+  };
+  export const deleteBoatLocation = (id) => {
+    const url = `${API_URL}/vouchers/deleteBoatLocation/${id}`;
+    return axios.delete (url, {
+      headers: options,
+    });
+  };
+  export const deleteRestriction = (id) => {
+    const url = `${API_URL}/vouchers/deleteRestriction/${id}`;
     return axios.delete (url, {
       headers: options,
     });
